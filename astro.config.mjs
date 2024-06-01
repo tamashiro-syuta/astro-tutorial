214ad02e116ba4c6f8d8b00866cb1a4d2bf0dfe1
tamashiro-syuta <syuta5825@gmail.com>
2024-06-02 00:06:21 +0900

    ✨ Astroアイランドを定義

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://tamasyu-astro-tutorial.netlify.app/"
+  site: "https://tamasyu-astro-tutorial.netlify.app/",
+  integrations: [preact()]
 });
